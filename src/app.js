1789aced38f49b44bb9f1cb333b2a768d8fd5f66
perlaconde <158175286+perlaconde@users.noreply.github.com>
2024-03-07 15:24:24 +0000

    message

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function numeroAleatorio(numero) {
+    return 1 * Math.floor(Math.random() * numero.length);
+  }
+
+  let whoR = who[numeroAleatorio(who)];
+  let actionR = action[numeroAleatorio(action)];
+  let whatR = what[numeroAleatorio(what)];
+  let whenR = when[numeroAleatorio(when)];
+  console.log(whoR + " " + actionR + " " + whatR + " " + whenR);
+  let excuseGenerated = whoR + " " + actionR + " " + whatR + " " + whenR;
+  const excuse = document.getElementById("excuse");
+  console.log(excuse);
+  return (excuse.innerHTML = excuseGenerated);
 };
